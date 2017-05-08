f456b0bce4b1831f812c747e8e07296642aa414c
MisterMX <mbxd12@web.de>
2017-05-08 20:25:42 +0200

    Add Hibernate entity classes

---
+++ com.igt.database.wrapper/database.wrapper/src/main/java/com/igt/database/wrapper/BillEntity.java
@@ -0,0 +1,65 @@
+package com.igt.database.wrapper;
+
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.Field;
+
+public class BillEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Field(analyze = Analyze.NO)
+	private int totalValue;
+	
+	@Field(analyze = Analyze.NO)
+	private Date purchaseDate;
+	
+	@OneToMany
+	private CustomerEntity customer;
+
+	
+	public BillEntity() {
+		
+	}
+	
+	public BillEntity(Long id, int totalValue, Date purchaseDate, CustomerEntity customer) {
+		super();
+		this.id = id;
+		this.totalValue = totalValue;
+		this.purchaseDate = purchaseDate;
+		this.customer = customer;
+	}
+	
+	
+	public CustomerEntity getCustomer() {
+		return customer;
+	}
+	public void setCustomer(CustomerEntity customer) {
+		this.customer = customer;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public int getTotalValue() {
+		return totalValue;
+	}
+	public void setTotalValue(int totalValue) {
+		this.totalValue = totalValue;
+	}
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+}
---
+++ com.igt.database.wrapper/database.wrapper/src/main/java/com/igt/database/wrapper/CustomerEntity.java
@@ -0,0 +1,76 @@
+package com.igt.database.wrapper;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
+
+@Entity
+@Indexed
+public class CustomerEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Field(analyze = Analyze.NO)
+	private String surName, foreName;
+	
+	@Field(analyze = Analyze.NO)
+	private Date birthDate;
+	
+	@ManyToOne
+	private Set<BillEntity> bills;
+	
+	
+	public CustomerEntity() {
+		
+	}
+	
+	public CustomerEntity(Long id, String surName, String foreName, Date birthDate, Set<BillEntity> bills) {
+		this.id = id;
+		this.surName = surName;
+		this.foreName = foreName;
+		this.birthDate = birthDate;
+		this.bills = bills;
+	}
+
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getSurName() {
+		return surName;
+	}
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+	public String getForeName() {
+		return foreName;
+	}
+	public void setForeName(String foreName) {
+		this.foreName = foreName;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public Set<BillEntity> getBills() {
+		return bills;
+	}
+	public void setBills(Set<BillEntity> bills) {
+		this.bills = bills;
+	}
+}
